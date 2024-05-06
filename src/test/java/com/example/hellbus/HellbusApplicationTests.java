5aec953ea7c3d118d866fb199badc0c4ed8f20f0
johnny-gill <uint777@gmail.com>
2024-05-06 16:52:09 +0900

    first commit

---
+++ src/test/java/com/example/hellbus/HellbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellbusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
